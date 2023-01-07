c4c3ab915ffa03a5c1b97e6c2296a160ca90e387
ijlee2 <ijlee2@users.noreply.github.com>
2023-01-07 14:47:03 +0100

    Allowed the end-user to provide codemod options

--- bin/ember-codemod-v1-to-v2.js
+++ bin/ember-codemod-v1-to-v2.js
@@ -1,8 +1,42 @@
 #!/usr/bin/env node
 'use strict';
 
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
+
 import { runCodemod } from '../src/index.js';
 
-const options = {};
+// Provide a title to the process in `ps`
+process.title = 'ember-codemod-v1-to-v2';
+
+// Set codemod options
+const { argv } = yargs(hideBin(process.argv))
+  .option('addon-location', {
+    describe: 'Location of the addon package',
+    type: 'string',
+  })
+  .option('root', {
+    describe: 'Location of your Ember addon',
+    type: 'string',
+  })
+  .option('test-app-location', {
+    describe: 'Location of the test-app package',
+    type: 'string',
+  })
+  .option('test-app-name', {
+    describe: 'Name of the test-app package',
+    type: 'string',
+  });
+
+function castEmptyStringToUndefined(string) {
+  return string === '' ? undefined : string;
+}
+
+const options = {
+  addonLocation: castEmptyStringToUndefined(argv['addon-location']),
+  projectRoot: argv['root'] ?? process.cwd(),
+  testAppLocation: castEmptyStringToUndefined(argv['test-app-location']),
+  testAppName: castEmptyStringToUndefined(argv['test-app-name']),
+};
 
 runCodemod(options);
