1df7d28604a82bde4729299f57d58d722c8efe6d
ijlee2 <ijlee2@users.noreply.github.com>
2023-01-16 13:26:00 +0100

    chore: Updated test fixtures

--- tests/fixtures/new-v1-addon-typescript/output/new-v1-addon/rollup.config.mjs
+++ tests/fixtures/new-v1-addon-typescript/output/new-v1-addon/rollup.config.mjs
@@ -15,12 +15,12 @@ export default {
   plugins: [
     // These are the modules that users should be able to import from your
     // addon. Anything not listed here may get optimized away.
-    addon.publicEntrypoints(['components/**/*.js', 'index.js']),
+    addon.publicEntrypoints([]),
 
     // These are the modules that should get reexported into the traditional
     // "app" tree. Things in here should also be in publicEntrypoints above, but
     // not everything in publicEntrypoints necessarily needs to go here.
-    addon.appReexports(['components/**/*.js']),
+    addon.appReexports([]),
 
     // Follow the V2 Addon rules about dependencies. Your code can import from
     // `dependencies` and `peerDependencies` as well as standard Ember-provided
