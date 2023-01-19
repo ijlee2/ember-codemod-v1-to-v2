10c4b41d13fc24d4a8a7fd48ebc308dbfa8841ef
ijlee2 <ijlee2@users.noreply.github.com>
2023-01-19 09:27:39 +0100

    refactor: Added tests for moveTestAppFiles

---
+++ tests/migration/ember-addon/steps/move-test-app-files/javascript.test.js
@@ -0,0 +1,152 @@
+import { moveTestAppFiles } from '../../../../../src/migration/ember-addon/steps/move-test-app-files.js';
+import {
+  augmentedOptions,
+  options,
+} from '../../../../helpers/shared-test-setups/javascript.js';
+import {
+  assertFixture,
+  loadFixture,
+  test,
+} from '../../../../helpers/testing.js';
+
+test('migration | ember-addon | steps | move-test-app-files > javascript', function () {
+  const inputProject = {
+    tests: {
+      acceptance: {
+        album: {
+          'accessibility-test.js': '',
+          'visual-regression-test.js': '',
+        },
+      },
+      dummy: {
+        app: {
+          components: {
+            '.gitkeep': '',
+          },
+          routes: {
+            '.gitkeep': '',
+          },
+          templates: {
+            '.gitkeep': '',
+          },
+          'app.js': '',
+          'index.html': '',
+          'router.js': '',
+        },
+        config: {
+          'ember-cli-update.json': '',
+          'ember-try.js': '',
+          'environment.js': '',
+          'optional-features.json': '',
+          'targets.js': '',
+        },
+        public: {
+          assets: {
+            'favicon.png': '',
+          },
+          'robots.txt': '',
+        },
+      },
+      helpers: {
+        'index.js': '',
+      },
+      integration: {
+        components: {
+          'container-query': {
+            'dataAttributePrefix-test.js': '',
+            'debounce-test.js': '',
+            'features-test.js': '',
+            'splattributes-test.js': '',
+            'tagName-test.js': '',
+          },
+        },
+      },
+      unit: {
+        utils: {
+          components: {
+            widgets: {
+              'widget-2-test.js': '',
+              'widget-3-test.js': '',
+            },
+          },
+        },
+        '.gitkeep': '',
+      },
+      'index.html': '',
+      'test-helper.js': '',
+    },
+  };
+
+  const outputProject = {
+    'test-app': {
+      app: {
+        components: {
+          '.gitkeep': '',
+        },
+        routes: {
+          '.gitkeep': '',
+        },
+        templates: {
+          '.gitkeep': '',
+        },
+        'app.js': '',
+        'index.html': '',
+        'router.js': '',
+      },
+      config: {
+        'ember-cli-update.json': '',
+        'ember-try.js': '',
+        'environment.js': '',
+        'optional-features.json': '',
+        'targets.js': '',
+      },
+      public: {
+        assets: {
+          'favicon.png': '',
+        },
+        'robots.txt': '',
+      },
+      tests: {
+        acceptance: {
+          album: {
+            'accessibility-test.js': '',
+            'visual-regression-test.js': '',
+          },
+        },
+        helpers: {
+          'index.js': '',
+        },
+        integration: {
+          components: {
+            'container-query': {
+              'dataAttributePrefix-test.js': '',
+              'debounce-test.js': '',
+              'features-test.js': '',
+              'splattributes-test.js': '',
+              'tagName-test.js': '',
+            },
+          },
+        },
+        unit: {
+          utils: {
+            components: {
+              widgets: {
+                'widget-2-test.js': '',
+                'widget-3-test.js': '',
+              },
+            },
+          },
+          '.gitkeep': '',
+        },
+        'index.html': '',
+        'test-helper.js': '',
+      },
+    },
+  };
+
+  loadFixture(inputProject, options);
+
+  moveTestAppFiles(augmentedOptions);
+
+  assertFixture(outputProject, options);
+});
