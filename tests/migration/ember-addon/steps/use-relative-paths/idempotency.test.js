157f14aa1e7f745a4efddf39380e2cdbb59d0e49
ijlee2 <ijlee2@users.noreply.github.com>
2023-01-19 08:25:49 +0100

    refactor: Asserted that useRelativePaths is idempotent, i.e. it doesn't alter existing relative paths

---
+++ tests/migration/ember-addon/steps/use-relative-paths/idempotency.test.js
@@ -0,0 +1,147 @@
+import { useRelativePaths } from '../../../../../src/migration/ember-addon/steps/use-relative-paths.js';
+import {
+  augmentedOptions,
+  options,
+} from '../../../../helpers/shared-test-setups/typescript.js';
+import {
+  assertFixture,
+  loadFixture,
+  test,
+} from '../../../../helpers/testing.js';
+
+const fileMapping = new Map([
+  [
+    'addon/components/container-query.ts',
+    {
+      input: [
+        `import { action } from '@ember/object';`,
+        `import Component from '@glimmer/component';`,
+        `import { tracked } from '@glimmer/tracking';`,
+        `import type {`,
+        `  Dimensions,`,
+        `  Features,`,
+        `  IndexSignatureParameter,`,
+        `  QueryResults,`,
+        `} from '../modifiers/container-query';`,
+      ].join('\n'),
+
+      output: [
+        `import { action } from '@ember/object';`,
+        `import Component from '@glimmer/component';`,
+        `import { tracked } from '@glimmer/tracking';`,
+        `import type {`,
+        `  Dimensions,`,
+        `  Features,`,
+        `  IndexSignatureParameter,`,
+        `  QueryResults,`,
+        `} from '../modifiers/container-query';`,
+      ].join('\n'),
+    },
+  ],
+
+  [
+    'tests/dummy/app/components/widgets/widget-3/tour-schedule/responsive-image.ts',
+    {
+      input: [
+        `import { action } from '@ember/object';`,
+        `import Component from '@glimmer/component';`,
+        `import { tracked } from '@glimmer/tracking';`,
+        `import type { Image } from '../../../../data/concert';`,
+        `import { findBestFittingImage } from '../../../../utils/components/widgets/widget-3';`,
+        `import type { Dimensions } from 'ember-container-query/modifiers/container-query';`,
+      ].join('\n'),
+
+      output: [
+        `import { action } from '@ember/object';`,
+        `import Component from '@glimmer/component';`,
+        `import { tracked } from '@glimmer/tracking';`,
+        `import type { Image } from '../../../../data/concert';`,
+        `import { findBestFittingImage } from '../../../../utils/components/widgets/widget-3';`,
+        `import type { Dimensions } from 'ember-container-query/modifiers/container-query';`,
+      ].join('\n'),
+    },
+  ],
+
+  [
+    'tests/dummy/app/app.ts',
+    {
+      input: [
+        `import Application from '@ember/application';`,
+        `import config from './config/environment';`,
+        `import loadInitializers from 'ember-load-initializers';`,
+        `import Resolver from 'ember-resolver';`,
+      ].join('\n'),
+
+      output: [
+        `import Application from '@ember/application';`,
+        `import config from './config/environment';`,
+        `import loadInitializers from 'ember-load-initializers';`,
+        `import Resolver from 'ember-resolver';`,
+      ].join('\n'),
+    },
+  ],
+]);
+
+test('migration | ember-addon | steps | use-relative-paths > idempotency', function () {
+  const inputProject = {
+    addon: {
+      components: {
+        'container-query.ts': fileMapping.get(
+          'addon/components/container-query.ts'
+        ).input,
+      },
+    },
+    tests: {
+      dummy: {
+        app: {
+          components: {
+            widgets: {
+              'widget-3': {
+                'tour-schedule': {
+                  'responsive-image.ts': fileMapping.get(
+                    'tests/dummy/app/components/widgets/widget-3/tour-schedule/responsive-image.ts'
+                  ).input,
+                },
+              },
+            },
+          },
+          'app.ts': fileMapping.get('tests/dummy/app/app.ts').input,
+        },
+      },
+    },
+  };
+
+  const outputProject = {
+    addon: {
+      components: {
+        'container-query.ts': fileMapping.get(
+          'addon/components/container-query.ts'
+        ).output,
+      },
+    },
+    tests: {
+      dummy: {
+        app: {
+          components: {
+            widgets: {
+              'widget-3': {
+                'tour-schedule': {
+                  'responsive-image.ts': fileMapping.get(
+                    'tests/dummy/app/components/widgets/widget-3/tour-schedule/responsive-image.ts'
+                  ).output,
+                },
+              },
+            },
+          },
+          'app.ts': fileMapping.get('tests/dummy/app/app.ts').output,
+        },
+      },
+    },
+  };
+
+  loadFixture(inputProject, options);
+
+  useRelativePaths(augmentedOptions);
+
+  assertFixture(outputProject, options);
+});
