ad79bc09855cddad555dc39f171639be7949ea3d
ijlee2 <ijlee2@users.noreply.github.com>
2023-01-19 15:53:42 +0100

    refactor: Used test fixtures to test the useRelativePaths step

--- tests/migration/ember-addon/steps/use-relative-paths/idempotency.test.js
+++ tests/migration/ember-addon/steps/use-relative-paths/idempotency.test.js
@@ -1,4 +1,5 @@
 import { useRelativePaths } from '../../../../../src/migration/ember-addon/steps/use-relative-paths.js';
+import { convertToJson } from '../../../../helpers/fixture.js';
 import {
   augmentedOptions,
   options,
@@ -9,135 +10,14 @@ import {
   test,
 } from '../../../../helpers/testing.js';
 
-const fileMapping = new Map([
-  [
-    'addon/components/container-query.ts',
-    {
-      input: [
-        `import { action } from '@ember/object';`,
-        `import Component from '@glimmer/component';`,
-        `import { tracked } from '@glimmer/tracking';`,
-        `import type {`,
-        `  Dimensions,`,
-        `  Features,`,
-        `  IndexSignatureParameter,`,
-        `  QueryResults,`,
-        `} from '../modifiers/container-query';`,
-      ].join('\n'),
-
-      output: [
-        `import { action } from '@ember/object';`,
-        `import Component from '@glimmer/component';`,
-        `import { tracked } from '@glimmer/tracking';`,
-        `import type {`,
-        `  Dimensions,`,
-        `  Features,`,
-        `  IndexSignatureParameter,`,
-        `  QueryResults,`,
-        `} from '../modifiers/container-query';`,
-      ].join('\n'),
-    },
-  ],
-
-  [
-    'tests/dummy/app/components/widgets/widget-3/tour-schedule/responsive-image.ts',
-    {
-      input: [
-        `import { action } from '@ember/object';`,
-        `import Component from '@glimmer/component';`,
-        `import { tracked } from '@glimmer/tracking';`,
-        `import type { Image } from '../../../../data/concert';`,
-        `import { findBestFittingImage } from '../../../../utils/components/widgets/widget-3';`,
-        `import type { Dimensions } from 'ember-container-query/modifiers/container-query';`,
-      ].join('\n'),
-
-      output: [
-        `import { action } from '@ember/object';`,
-        `import Component from '@glimmer/component';`,
-        `import { tracked } from '@glimmer/tracking';`,
-        `import type { Image } from '../../../../data/concert';`,
-        `import { findBestFittingImage } from '../../../../utils/components/widgets/widget-3';`,
-        `import type { Dimensions } from 'ember-container-query/modifiers/container-query';`,
-      ].join('\n'),
-    },
-  ],
-
-  [
-    'tests/dummy/app/app.ts',
-    {
-      input: [
-        `import Application from '@ember/application';`,
-        `import config from './config/environment';`,
-        `import loadInitializers from 'ember-load-initializers';`,
-        `import Resolver from 'ember-resolver';`,
-      ].join('\n'),
-
-      output: [
-        `import Application from '@ember/application';`,
-        `import config from './config/environment';`,
-        `import loadInitializers from 'ember-load-initializers';`,
-        `import Resolver from 'ember-resolver';`,
-      ].join('\n'),
-    },
-  ],
-]);
-
 test('migration | ember-addon | steps | use-relative-paths > idempotency', function () {
-  const inputProject = {
-    addon: {
-      components: {
-        'container-query.ts': fileMapping.get(
-          'addon/components/container-query.ts'
-        ).input,
-      },
-    },
-    tests: {
-      dummy: {
-        app: {
-          components: {
-            widgets: {
-              'widget-3': {
-                'tour-schedule': {
-                  'responsive-image.ts': fileMapping.get(
-                    'tests/dummy/app/components/widgets/widget-3/tour-schedule/responsive-image.ts'
-                  ).input,
-                },
-              },
-            },
-          },
-          'app.ts': fileMapping.get('tests/dummy/app/app.ts').input,
-        },
-      },
-    },
-  };
+  const inputProject = convertToJson(
+    'steps/use-relative-paths/typescript/output'
+  );
 
-  const outputProject = {
-    addon: {
-      components: {
-        'container-query.ts': fileMapping.get(
-          'addon/components/container-query.ts'
-        ).output,
-      },
-    },
-    tests: {
-      dummy: {
-        app: {
-          components: {
-            widgets: {
-              'widget-3': {
-                'tour-schedule': {
-                  'responsive-image.ts': fileMapping.get(
-                    'tests/dummy/app/components/widgets/widget-3/tour-schedule/responsive-image.ts'
-                  ).output,
-                },
-              },
-            },
-          },
-          'app.ts': fileMapping.get('tests/dummy/app/app.ts').output,
-        },
-      },
-    },
-  };
+  const outputProject = convertToJson(
+    'steps/use-relative-paths/typescript/output'
+  );
 
   loadFixture(inputProject, options);
 
