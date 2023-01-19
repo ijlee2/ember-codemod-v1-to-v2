2d866113b3ea44b8022d8cc482346bcc21039a7d
ijlee2 <ijlee2@users.noreply.github.com>
2023-01-19 15:50:40 +0100

    refactor: Added tests for createFilesFromBlueprint

---
+++ tests/migration/ember-addon/steps/create-files-from-blueprint/customizations.test.js
@@ -0,0 +1,44 @@
+import { createFilesFromBlueprint } from '../../../../../src/migration/ember-addon/steps/create-files-from-blueprint.js';
+import { convertToJson } from '../../../../helpers/fixture.js';
+import {
+  augmentedOptions,
+  options,
+} from '../../../../helpers/shared-test-setups/customizations.js';
+import {
+  assertFixture,
+  loadFixture,
+  test,
+} from '../../../../helpers/testing.js';
+
+test('migration | ember-addon | steps | create-files-from-blueprint > customizations', function () {
+  const inputProject = {};
+
+  const outputProject = convertToJson(
+    'steps/create-files-from-blueprint/customizations/output'
+  );
+
+  loadFixture(inputProject, options);
+
+  const context = {
+    appReexports: [
+      'components/container-query.js',
+      'helpers/aspect-ratio.js',
+      'helpers/height.js',
+      'helpers/width.js',
+      'modifiers/container-query.js',
+    ],
+    publicEntrypoints: [
+      'components/container-query.js',
+      'helpers/aspect-ratio.js',
+      'helpers/height.js',
+      'helpers/width.js',
+      'index.js',
+      'modifiers/container-query.js',
+      'template-registry.js',
+    ],
+  };
+
+  createFilesFromBlueprint(context, augmentedOptions);
+
+  assertFixture(outputProject, options);
+});
