c05853f2158fcaa1da1ac400e1815abbe90a8cb0
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2023-01-31 23:01:48 +0100

    Updated latestVersions and refactored code (#15)
    * chore: Updated latestVersions
    
    * chore: Updated test fixtures
    
    * refactor: Practiced early exits. Threw an error to catch development mistakes.
    
    ---------
    
    Co-authored-by: ijlee2 <ijlee2@users.noreply.github.com>

--- src/utils/blueprints/decide-version.js
+++ src/utils/blueprints/decide-version.js
@@ -1,26 +1,37 @@
 const latestVersions = new Map([
   ['@babel/core', '7.20.12'],
   ['@babel/plugin-proposal-class-properties', '7.18.6'],
-  ['@babel/plugin-proposal-decorators', '7.20.7'],
+  ['@babel/plugin-proposal-decorators', '7.20.13'],
   ['@babel/preset-typescript', '7.18.6'],
   ['@embroider/addon-dev', '3.0.0'],
   ['@embroider/addon-shim', '1.8.4'],
   ['@rollup/plugin-babel', '6.0.3'],
   ['concurrently', '7.6.0'],
-  ['ember-auto-import', '2.5.0'],
+  ['ember-auto-import', '2.6.0'],
   ['ember-cli-babel', '7.26.11'],
-  ['ember-cli-htmlbars', '6.1.11'],
+  ['ember-cli-htmlbars', '6.2.0'],
   ['prettier', '2.8.3'],
-  ['rollup', '3.10.0'],
+  ['rollup', '3.12.0'],
   ['rollup-plugin-copy', '3.4.0'],
-  ['rollup-plugin-ts', '3.1.1'],
+  ['rollup-plugin-ts', '3.2.0'],
 ]);
 
 export function decideVersion(packageName, options) {
   const { packages } = options;
 
   const installedVersion = packages.addon.dependencies.get(packageName);
-  const latestVersion = `^${latestVersions.get(packageName)}`;
 
-  return installedVersion ?? latestVersion;
+  if (installedVersion) {
+    return installedVersion;
+  }
+
+  const latestVersion = latestVersions.get(packageName);
+
+  if (!latestVersion) {
+    throw new RangeError(
+      `ERROR: The latest version of \`${packageName}\` is unknown.\n`
+    );
+  }
+
+  return `^${latestVersion}`;
 }
--- tests/utils/blueprints/decide-version.test.js
+++ tests/utils/blueprints/decide-version.test.js
@@ -8,8 +8,24 @@ test('utils | blueprints | decide-version > package is installed', function () {
   assert.strictEqual(version, '^2.8.1');
 });
 
-test('utils | blueprints | decide-version > package is not installed', function () {
+test('utils | blueprints | decide-version > package is not installed and we provide the latest version', function () {
   const version = decideVersion('rollup', options);
 
-  assert.strictEqual(version, '^3.10.0');
+  assert.strictEqual(version, '^3.12.0');
+});
+
+test('utils | blueprints | decide-version > package is not installed and we forgot to provide the latest version', function () {
+  assert.throws(
+    () => {
+      decideVersion('some-package-not-part-of-blueprint', options);
+    },
+    (error) => {
+      assert.strictEqual(
+        error.message,
+        'ERROR: The latest version of `some-package-not-part-of-blueprint` is unknown.\n'
+      );
+
+      return true;
+    }
+  );
 });
