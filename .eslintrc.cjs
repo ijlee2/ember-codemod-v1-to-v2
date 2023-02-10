f03bf0d86b80ba84e135baadb316d5c4ed41fd4d
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2023-02-10 05:38:36 +0100

    chore: Installed eslint-plugin-n and eslint-plugin-import. Updated prettier. (#18)
    Co-authored-by: ijlee2 <ijlee2@users.noreply.github.com>

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,7 +9,12 @@ module.exports = {
     sourceType: 'module',
   },
   plugins: ['simple-import-sort'],
-  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:import/recommended',
+    'plugin:n/recommended',
+    'plugin:prettier/recommended',
+  ],
   rules: {
     curly: 'error',
     'simple-import-sort/exports': 'error',
