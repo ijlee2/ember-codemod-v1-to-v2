d85bb1621e5a28bad7d3e509aeda676357ed820e
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2023-03-04 10:17:58 +0100

    Move packages from dependencies to devDependencies only when the package exists  (#21)
    * refactor: Removed ternary operators (deeply nested code) to improve code readability
    
    * chore: Sorted packages that are to be installed
    
    * bugfix: Updated moveDependenciesToDevDependencies to move only the packages that exist already
    
    * chore: Updated latestVersions
    
    * chore: Updated test fixtures
    
    ---------
    
    Co-authored-by: ijlee2 <ijlee2@users.noreply.github.com>

--- src/migration/ember-addon/steps/update-test-app-package-json.js
+++ src/migration/ember-addon/steps/update-test-app-package-json.js
@@ -10,22 +10,26 @@ function moveDependenciesToDevDependencies(packageJson, options) {
   const dependencies = convertToMap(packageJson['dependencies']);
   const devDependencies = convertToMap(packageJson['devDependencies']);
 
-  const packagesToMove = packages.addon.hasTypeScript
-    ? [
-        'ember-auto-import',
-        'ember-cli-babel',
-        'ember-cli-htmlbars',
-        'ember-cli-typescript',
-      ]
-    : ['ember-auto-import', 'ember-cli-babel', 'ember-cli-htmlbars'];
+  const packagesToMove = new Set([
+    'ember-auto-import',
+    'ember-cli-babel',
+    'ember-cli-htmlbars',
+  ]);
 
-  packagesToMove.forEach((packageName) => {
-    const version = decideVersion(packageName, options);
+  if (packages.addon.hasTypeScript) {
+    packagesToMove.add('ember-cli-typescript');
+  }
 
-    devDependencies.set(packageName, version);
+  [...packagesToMove]
+    .filter((packageName) => dependencies.has(packageName))
+    .sort()
+    .forEach((packageName) => {
+      const version = decideVersion(packageName, options);
 
-    dependencies.delete(packageName);
-  });
+      devDependencies.set(packageName, version);
+
+      dependencies.delete(packageName);
+    });
 
   packageJson['dependencies'] = convertToObject(dependencies);
   packageJson['devDependencies'] = convertToObject(devDependencies);
