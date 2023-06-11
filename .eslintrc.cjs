86d960fa5e66a70d0ef1f647cedf614b0ea7c8be
ijlee2 <ijlee2@users.noreply.github.com>
2023-06-11 18:24:14 +0200

    chore: Configured eslint and prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,27 +2,58 @@
 
 module.exports = {
   root: true,
-  parser: '@babel/eslint-parser',
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
-    requireConfigFile: false,
     sourceType: 'module',
   },
-  plugins: ['simple-import-sort'],
+  plugins: ['@typescript-eslint', 'simple-import-sort', 'typescript-sort-keys'],
   extends: [
     'eslint:recommended',
     'plugin:import/recommended',
+    'plugin:import/typescript',
     'plugin:n/recommended',
     'plugin:prettier/recommended',
+    'plugin:typescript-sort-keys/recommended',
   ],
   rules: {
     curly: 'error',
     'simple-import-sort/exports': 'error',
     'simple-import-sort/imports': 'error',
   },
-  env: {
-    es6: true,
-    node: true,
+  settings: {
+    'import/resolver': {
+      node: true,
+      typescript: true,
+    },
   },
-  overrides: [],
+  overrides: [
+    // TypeScript files
+    {
+      files: ['**/*.{cts,ts}'],
+      extends: [
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended',
+      ],
+      rules: {
+        '@typescript-eslint/array-type': 'error',
+      },
+    },
+    // TypeScript and JavaScript files
+    {
+      files: ['**/*.{cjs,cts,js,ts}'],
+      rules: {
+        'import/no-duplicates': 'error',
+      },
+    },
+    // Node files
+    {
+      files: ['./.eslintrc.{cjs,js}', './.prettierrc.{cjs,js}'],
+      env: {
+        browser: false,
+        node: true,
+      },
+      extends: ['plugin:n/recommended'],
+    },
+  ],
 };
