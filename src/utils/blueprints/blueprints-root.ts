dc99e123f6f9cbfe308658c3e1c30355fe6bdef4
ijlee2 <ijlee2@users.noreply.github.com>
2023-06-12 15:58:25 +0200

    bugfix: Prevented typescript from compiling or type-checking blueprints

--- src/utils/blueprints/blueprints-root.ts
+++ src/utils/blueprints/blueprints-root.ts
@@ -3,6 +3,8 @@ import { join } from 'node:path';
 import { getFilePath } from '@codemod-utils/blueprints';
 
 const fileURL = import.meta.url;
-const srcDirectory = join(getFilePath(fileURL), '../..');
 
-export const blueprintsRoot: string = join(srcDirectory, 'blueprints');
+export const blueprintsRoot: string = join(
+  getFilePath(fileURL),
+  '../../../../blueprints',
+);
