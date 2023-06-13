a34e7a83b148051e5657bc7301553967bcef6baa
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2023-06-13 18:43:02 +0200

    Updated project structure (#43)
    * chore: Moved blueprints back to src/blueprints
    
    * chore: Configured project to account for the new location
    
    ---------
    
    Co-authored-by: ijlee2 <ijlee2@users.noreply.github.com>

--- tests/utils/blueprints/blueprints-root.test.ts
+++ tests/utils/blueprints/blueprints-root.test.ts
@@ -3,5 +3,5 @@ import { assert, test } from '@codemod-utils/tests';
 import { blueprintsRoot } from '../../../src/utils/blueprints.js';
 
 test('utils | blueprints | blueprints-root', function () {
-  assert.strictEqual(blueprintsRoot.endsWith('blueprints'), true);
+  assert.strictEqual(blueprintsRoot.endsWith('src/blueprints'), true);
 });
