21366eefb600ed7a0997028cabda9d4f98ca2976
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2023-06-13 20:07:39 +0200

    Wrote a shell script to simplify the build and test scripts (#44)
    * bugfix: Removed dist folder before compilation
    
    * refactor: Wrote a shell script to simplify the build and test scripts
    
    * bugfix: Made script POSIX sh compliant
    
    ---------
    
    Co-authored-by: ijlee2 <ijlee2@users.noreply.github.com>

---
+++ build.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env sh
+
+ENVIRONMENT=$1
+
+if [ $ENVIRONMENT = "--production" ]
+then
+  # Clean slate
+  rm -rf "dist"
+
+  # Compile TypeScript
+  tsc --project "tsconfig.build.json"
+
+  # Configure files
+  chmod +x "dist/bin/ember-codemod-v1-to-v2.js"
+  cp -r "src/blueprints" "dist/src/blueprints"
+
+  echo "SUCCESS: Built dist.\n"
+
+elif [ $ENVIRONMENT = "--test" ]
+then
+  # Clean slate
+  rm -rf "dist-for-testing"
+
+  # Compile TypeScript
+  tsc --project "tsconfig.json"
+
+  # Configure files
+  cp -r "src/blueprints" "dist-for-testing/src/blueprints"
+
+  echo "SUCCESS: Built dist-for-testing.\n"
+
+fi
