2614c48ec48a307509278dcaccb949ed5f66ccd7
ijlee2 <ijlee2@users.noreply.github.com>
2023-06-21 15:20:53 +0200

    chore: Standardized scripts

--- codemod-test-fixture.sh
+++ codemod-test-fixture.sh
@@ -4,34 +4,22 @@
 #
 #  A. Purpose
 #
-#    Fix the expected output of a test fixture after updating the source code
-#    of ember-codemod-v1-to-v2. 
+#    Fix the expected output of a test fixture after updating the source code.
 #
 #  B. Usage
 #
+#    For named arguments, do not include `=` between the flag and the value.
+#    Positional arguments are to appear at the end.
+#
 #    ./codemod-test-fixture.sh [OPTIONAL-FLAGS] <FIXTURE-NAME>
-#
-#    Step 1. Run the script to update the files in `tests/fixtures/<FIXTURE-NAME>/output`.
-#
-#      Choice a. Run the codemod without the optional arguments.
-#
-#        ./codemod-test-fixture.sh ember-container-query-glint
-#
-#      Choice b. Run the codemod with the optional arguments. (For named arguments,
-#      do not include `=` between the flag and the value. Positional arguments must
-#      appear at the end.)
-#
-#        ./codemod-test-fixture.sh -a "packages/ember-container-query" ember-container-query-customizations
 #
 #---------
 
 # Read the named arguments
-while getopts ":a:t:T:" flag
+while getopts ":a:" flag
 do
   case $flag in
-    a) ADDON_LOCATION=$OPTARG;;
-    t) TEST_APP_LOCATION=$OPTARG;;
-    T) TEST_APP_NAME=$OPTARG;;
+    a) ARGUMENTS=$OPTARG;;
   esac
 done
 
@@ -51,10 +39,6 @@ fi
 rm -r "tests/fixtures/$FIXTURE/output"
 cp -r "tests/fixtures/$FIXTURE/input" "tests/fixtures/$FIXTURE/output"
 
-./dist/bin/ember-codemod-v1-to-v2.js \
-  --addon-location=$ADDON_LOCATION \
-  --root="tests/fixtures/$FIXTURE/output" \
-  --test-app-location=$TEST_APP_LOCATION \
-  --test-app-name=$TEST_APP_NAME
+./dist/bin/ember-codemod-v1-to-v2.js $ARGUMENTS --root="tests/fixtures/$FIXTURE/output"
 
 echo "SUCCESS: Updated the output of $FIXTURE.\n"
