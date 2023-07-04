587caf10160a4a58e92c1d968e9a866bd8cc537f
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2023-07-04 10:39:12 +0200

    Updated latestVersions (#50)
    * chore: Updated latestVersions
    
    * chore: Updated test fixtures
    
    ---------
    
    Co-authored-by: ijlee2 <ijlee2@users.noreply.github.com>

--- src/utils/blueprints/get-version.ts
+++ src/utils/blueprints/get-version.ts
@@ -3,11 +3,11 @@ import { decideVersion } from '@codemod-utils/blueprints';
 import type { Options } from '../../types/index.js';
 
 const latestVersions = new Map([
-  ['@babel/core', '7.22.5'],
+  ['@babel/core', '7.22.6'],
   ['@babel/plugin-proposal-class-properties', '7.18.6'],
-  ['@babel/plugin-proposal-decorators', '7.22.5'],
+  ['@babel/plugin-proposal-decorators', '7.22.6'],
   ['@babel/preset-typescript', '7.22.5'],
-  ['@babel/runtime', '7.22.5'],
+  ['@babel/runtime', '7.22.6'],
   ['@embroider/addon-dev', '3.1.1'],
   ['@embroider/addon-shim', '1.8.5'],
   ['@rollup/plugin-babel', '6.0.3'],
@@ -15,7 +15,7 @@ const latestVersions = new Map([
   ['ember-auto-import', '2.6.3'],
   ['ember-cli-babel', '7.26.11'],
   ['ember-cli-htmlbars', '6.2.0'],
-  ['rollup', '3.24.1'],
+  ['rollup', '3.26.0'],
   ['rollup-plugin-copy', '3.4.0'],
   ['rollup-plugin-ts', '3.2.0'],
 ]);
