8ee164c9ec051fadac76119113808d62b3616cbb
ijlee2 <ijlee2@users.noreply.github.com>
2023-07-06 08:59:22 +0200

    refactor: Extracted functions (update-other-fields)

--- src/migration/ember-addon/steps/update-addon-package-json/update-other-fields.ts
+++ src/migration/ember-addon/steps/update-addon-package-json/update-other-fields.ts
@@ -1,64 +1,65 @@
 import type { Context, Options, PackageJson } from '../../../../types/index.js';
 
-export function updateOtherFields(
-  packageJson: PackageJson,
-  context: Context,
-  options: Options,
-): void {
-  const { packages } = options;
-  const hasPublicAssets = context.addon.publicAssets.length > 0;
+type Data = {
+  hasPublicAssets: boolean;
+  hasTypeScript: boolean;
+  publicAssetMapping: Record<string, string>;
+};
 
-  if (hasPublicAssets) {
-    const publicAssetMapping = context.addon.publicAssets.reduce(
-      (accumulator, filePath) => {
-        const from = `./public/${filePath}`;
-        const to = `/${packages.addon.name}/${filePath}`;
+function updateEmberAddon(packageJson: PackageJson, data: Data): void {
+  const { hasPublicAssets, publicAssetMapping } = data;
 
-        accumulator[from] = to;
-
-        return accumulator;
-      },
-      {} as Record<string, string>,
-    );
-
-    packageJson['ember-addon'] = {
-      'app-js': {},
-      main: 'addon-main.cjs',
-      'public-assets': publicAssetMapping,
-      type: 'addon',
-      version: 2,
-    };
-  } else {
+  if (!hasPublicAssets) {
     packageJson['ember-addon'] = {
       'app-js': {},
       main: 'addon-main.cjs',
       type: 'addon',
       version: 2,
     };
+
+    return;
   }
 
-  if (packages.addon.hasTypeScript) {
-    packageJson['exports'] = {
-      '.': {
-        types: './declarations/index.d.ts',
-        default: './dist/index.js',
-      },
-      './*': {
-        /*
-          This object has an order dependency. The `default` key must appear last.
-        */
-        types: './declarations/*.d.ts',
-        default: './dist/*.js',
-      },
-      './addon-main.js': './addon-main.cjs',
-    };
-  } else {
+  packageJson['ember-addon'] = {
+    'app-js': {},
+    main: 'addon-main.cjs',
+    'public-assets': publicAssetMapping,
+    type: 'addon',
+    version: 2,
+  };
+}
+
+function updateExports(packageJson: PackageJson, data: Data): void {
+  const { hasTypeScript } = data;
+
+  if (!hasTypeScript) {
     packageJson['exports'] = {
       '.': './dist/index.js',
       './*': './dist/*.js',
       './addon-main.js': './addon-main.cjs',
     };
+
+    return;
   }
+
+  packageJson['exports'] = {
+    '.': {
+      types: './declarations/index.d.ts',
+      default: './dist/index.js',
+    },
+    './*': {
+      /*
+        This object has an order dependency. The `default` key must appear last.
+      */
+      types: './declarations/*.d.ts',
+      default: './dist/*.js',
+    },
+    './addon-main.js': './addon-main.cjs',
+  };
+}
+
+function updateFiles(packageJson: PackageJson, data: Data): void {
+  const { hasPublicAssets, hasTypeScript } = data;
 
   const files = new Set(['addon-main.cjs', 'dist']);
 
@@ -66,17 +67,56 @@ export function updateOtherFields(
     files.add('public');
   }
 
-  if (packages.addon.hasTypeScript) {
+  if (hasTypeScript) {
     files.add('declarations');
   }
 
   packageJson['files'] = Array.from(files).sort();
+}
 
-  if (packages.addon.hasTypeScript) {
-    packageJson['typesVersions'] = {
-      '*': {
-        '*': ['declarations/*'],
-      },
-    };
+function updateTypesVersions(packageJson: PackageJson, data: Data): void {
+  const { hasTypeScript } = data;
+
+  if (!hasTypeScript) {
+    return;
   }
+
+  packageJson['typesVersions'] = {
+    '*': {
+      '*': ['declarations/*'],
+    },
+  };
+}
+
+export function updateOtherFields(
+  packageJson: PackageJson,
+  context: Context,
+  options: Options,
+): void {
+  const { packages } = options;
+
+  const publicAssetMapping = context.addon.publicAssets.reduce(
+    (accumulator, filePath) => {
+      const from = `./public/${filePath}`;
+      const to = `/${packages.addon.name}/${filePath}`;
+
+      accumulator[from] = to;
+
+      return accumulator;
+    },
+    {} as Record<string, string>,
+  );
+
+  const hasPublicAssets = Object.keys(publicAssetMapping).length > 0;
+
+  const data = {
+    hasPublicAssets,
+    hasTypeScript: packages.addon.hasTypeScript,
+    publicAssetMapping,
+  };
+
+  updateEmberAddon(packageJson, data);
+  updateExports(packageJson, data);
+  updateFiles(packageJson, data);
+  updateTypesVersions(packageJson, data);
 }
