83394c425839fce71139b1603cc847b7b346ab1c
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2023-08-12 10:01:36 +0200

    Updated blueprints to match @embroider/addon-blueprint@2.2.0 (#62)
    * chore: Updated blueprints
    
    * chore: Updated fixtures
    
    * chore: Updated latestVersions
    
    * chore: Updated fixtures
    
    ---------
    
    Co-authored-by: ijlee2 <ijlee2@users.noreply.github.com>

--- src/utils/blueprints/get-version.ts
+++ src/utils/blueprints/get-version.ts
@@ -3,19 +3,19 @@ import { decideVersion } from '@codemod-utils/blueprints';
 import type { Options } from '../../types/index.js';
 
 const latestVersions = new Map([
-  ['@babel/core', '7.22.6'],
-  ['@babel/plugin-proposal-decorators', '7.22.6'],
+  ['@babel/core', '7.22.10'],
+  ['@babel/plugin-proposal-decorators', '7.22.10'],
   ['@babel/plugin-transform-class-properties', '7.22.5'],
   ['@babel/preset-typescript', '7.22.5'],
-  ['@babel/runtime', '7.22.6'],
-  ['@embroider/addon-dev', '3.1.1'],
-  ['@embroider/addon-shim', '1.8.5'],
+  ['@babel/runtime', '7.22.10'],
+  ['@embroider/addon-dev', '4.1.0'],
+  ['@embroider/addon-shim', '1.8.6'],
   ['@rollup/plugin-babel', '6.0.3'],
   ['concurrently', '8.2.0'],
   ['ember-auto-import', '2.6.3'],
   ['ember-cli-babel', '7.26.11'],
-  ['ember-cli-htmlbars', '6.2.0'],
-  ['rollup', '3.26.0'],
+  ['ember-cli-htmlbars', '6.3.0'],
+  ['rollup', '3.28.0'],
   ['rollup-plugin-copy', '3.4.0'],
 ]);
 
