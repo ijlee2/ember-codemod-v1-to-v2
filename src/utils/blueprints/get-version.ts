2b996dd0dc023fd1598d7e43a43e61f03cc00e90
ijlee2 <ijlee2@users.noreply.github.com>
2023-12-20 17:30:24 +0100

    feature: Updated latestVersions

--- src/utils/blueprints/get-version.ts
+++ src/utils/blueprints/get-version.ts
@@ -3,23 +3,23 @@ import { decideVersion } from '@codemod-utils/blueprints';
 import type { Options } from '../../types/index.js';
 
 const latestVersions = new Map([
-  ['@babel/core', '7.23.2'],
-  ['@babel/plugin-transform-typescript', '7.22.15'],
-  ['@babel/runtime', '7.23.2'],
-  ['@embroider/addon-dev', '4.1.1'],
-  ['@embroider/addon-shim', '1.8.6'],
-  ['@embroider/test-setup', '3.0.2'],
+  ['@babel/core', '7.23.6'],
+  ['@babel/plugin-transform-typescript', '7.23.6'],
+  ['@babel/runtime', '7.23.6'],
+  ['@embroider/addon-dev', '4.1.3'],
+  ['@embroider/addon-shim', '1.8.7'],
+  ['@embroider/test-setup', '3.0.3'],
   ['@rollup/plugin-babel', '6.0.4'],
-  ['@tsconfig/ember', '3.0.2'],
+  ['@tsconfig/ember', '3.0.3'],
   ['babel-plugin-ember-template-compilation', '2.2.1'],
   ['concurrently', '8.2.2'],
   ['decorator-transforms', '1.0.1'],
-  ['ember-auto-import', '2.6.3'],
+  ['ember-auto-import', '2.7.1'],
   ['ember-cli-babel', '8.1.0'],
   ['ember-cli-htmlbars', '6.3.0'],
-  ['rollup', '4.3.0'],
+  ['rollup', '4.9.1'],
   ['rollup-plugin-copy', '3.5.0'],
-  ['typescript', '5.2.2'],
+  ['typescript', '5.3.3'],
 ]);
 
 export function getVersion(packageName: string, options: Options): string {
