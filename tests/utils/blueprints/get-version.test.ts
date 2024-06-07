d2b9da1a75419b31d00621b2a5b7659631b17f86
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2024-06-07 08:38:31 +0200

    Updated latestVersions (#85)
    * chore: Updated latestVersions
    
    * chore: Updated fixtures
    
    ---------
    
    Co-authored-by: ijlee2 <ijlee2@users.noreply.github.com>

--- tests/utils/blueprints/get-version.test.ts
+++ tests/utils/blueprints/get-version.test.ts
@@ -4,6 +4,6 @@ import { getLatestVersion, getVersion } from '../../../src/utils/blueprints.js';
 import { options } from '../../helpers/shared-test-setups/typescript.js';
 
 test('utils | blueprints | get-version', function () {
-  assert.strictEqual(getLatestVersion('prettier'), '^3.2.5');
+  assert.strictEqual(getLatestVersion('prettier'), '^3.3.1');
   assert.strictEqual(getVersion('prettier', options), '^2.8.1');
 });
