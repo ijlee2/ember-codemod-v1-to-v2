f326f8dd1b2f2a25c7a884b6814ec52bd90cd599
ijlee2 <ijlee2@users.noreply.github.com>
2025-01-15 09:04:27 +0100

    chore: Simplified types/global.d.ts for test-app

--- src/blueprints/ember-addon/__testAppLocation__/types/global.d.ts
+++ src/blueprints/ember-addon/__testAppLocation__/types/global.d.ts
@@ -1,7 +1,3 @@
-// Types for compiled templates
-declare module '<%= options.packages.testApp.name %>/templates/*' {
-  import { TemplateFactory } from 'ember-cli-htmlbars';
-
-  const tmpl: TemplateFactory;
-  export default tmpl;
-}
+<% if (options.packages.addon.hasGlint) { %>import '@glint/environment-ember-loose';
+import '@glint/environment-ember-template-imports';
+<% } %>
