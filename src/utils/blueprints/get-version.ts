2abded84f279756fb1f2457a242cda2c586d2a38
ijlee2 <ijlee2@users.noreply.github.com>
2025-03-25 10:07:07 +0100

    chore: Updated latestVersions

--- src/utils/blueprints/get-version.ts
+++ src/utils/blueprints/get-version.ts
@@ -3,10 +3,10 @@ import { decideVersion } from '@codemod-utils/blueprints';
 import type { Options } from '../../types/index.js';
 
 const latestVersions = new Map([
-  ['@babel/core', '7.26.0'],
-  ['@babel/plugin-transform-typescript', '7.26.5'],
-  ['@babel/runtime', '7.26.0'],
-  ['@embroider/addon-dev', '7.1.1'],
+  ['@babel/core', '7.26.10'],
+  ['@babel/plugin-transform-typescript', '7.26.8'],
+  ['@babel/runtime', '7.26.10'],
+  ['@embroider/addon-dev', '7.1.3'],
   ['@embroider/addon-shim', '1.9.0'],
   ['@embroider/broccoli-side-watch', '1.0.1'],
   ['@embroider/test-setup', '4.0.0'],
@@ -21,7 +21,7 @@ const latestVersions = new Map([
   ['@ijlee2-frontend-configs/stylelint', '0.2.3'],
   ['@ijlee2-frontend-configs/typescript', '0.4.0'],
   ['@rollup/plugin-babel', '6.0.4'],
-  ['babel-plugin-ember-template-compilation', '2.3.0'],
+  ['babel-plugin-ember-template-compilation', '2.4.0'],
   ['concurrently', '9.1.2'],
   ['decorator-transforms', '2.3.0'],
   ['ember-auto-import', '2.10.0'],
@@ -33,7 +33,7 @@ const latestVersions = new Map([
   ['ember-try', '4.0.0'],
   ['eslint', '9.23.0'],
   ['prettier', '3.5.3'],
-  ['rollup', '4.30.1'],
+  ['rollup', '4.37.0'],
   ['rollup-plugin-copy', '3.5.0'],
   ['stylelint', '16.16.0'],
   ['typescript', '5.8.2'],
