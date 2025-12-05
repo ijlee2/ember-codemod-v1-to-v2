0912926e674b551285a8b860dcf0ce8a971156a2
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2025-12-05 11:35:50 -0600

    chore: Downstreamed changes from @codemod-utils/cli@3.8.3

--- src/utils/blueprints/blueprints-root.ts
+++ src/utils/blueprints/blueprints-root.ts
@@ -4,7 +4,4 @@ import { getFilePath } from '@codemod-utils/blueprints';
 
 const fileURL = import.meta.url;
 
-export const blueprintsRoot: string = join(
-  getFilePath(fileURL),
-  '../../blueprints',
-);
+export const blueprintsRoot = join(getFilePath(fileURL), '../../blueprints');
